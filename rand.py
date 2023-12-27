f445b943ac5ce80f2a0005a583672b5385dcaae6
Amit Hirpara <hirparaamit43@gmail.com>
2023-12-27 17:33:18 +0530

    Update code

--- rand.py
+++ rand.py
@@ -1,26 +1,19 @@
+import os
 from flask import Flask, url_for, session, redirect, request, render_template
-import json
-import requests
 from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
-appConf = {
-    "OAUTH2_CLIENT_ID": "394024938730-cde4t7t88rb9eqr6cci4eiflls8rse1s.apps.googleusercontent.com",
-    "OAUTH2_CLIENT_SECRET": "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",
-    "OAUTH2_META_URL": "https://accounts.google.com/.well-known/openid-configuration",
-    "FLASK_SECRET": "0b0e2adc-82ae-4bc2-8b8f-95fb6da9139b",
-    "FLASK_PORT": 5000
-}
-app.secret_key = appConf.get("FLASK_SECRET")
+
+app.secret_key = os.getenv("FLASK_SECRET")
 oauth = OAuth(app)
 oauth.register(
     "myApp",
-    client_id=appConf.get("OAUTH2_CLIENT_ID"),
-    client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
+    client_id=os.getenv("OAUTH2_CLIENT_ID"),
+    client_secret=os.getenv("OAUTH2_CLIENT_SECRET"),
     client_kwargs={
         "scope": "https://www.googleapis.com/auth/youtube.upload",
     },
-    server_metadata_url=f'{appConf.get("OAUTH2_META_URL")}',
+    server_metadata_url=f'{os.getenv("OAUTH2_META_URL")}',
 )
 
 @app.route("/")
@@ -43,4 +36,4 @@ def logout():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(port=appConf.get("FLASK_PORT"), debug=True)
+    app.run(debug=True)
