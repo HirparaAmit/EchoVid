5b9bd13b03da582903fd03e3344bba7de8b9435f
Amit Hirpara <hirparaamit43@gmail.com>
2023-12-28 12:58:20 +0530

    Update code

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 import os
 from flask import Flask, url_for, session, redirect, request, render_template
 from authlib.integrations.flask_client import OAuth
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+from google.oauth2.credentials import Credentials
+from io import BytesIO
 
 app = Flask(__name__)
 
 app.secret_key = os.getenv("FLASK_SECRET")
 oauth = OAuth(app)
-oauth.register(
-    "myApp",
+google = oauth.register(
+    name='google',
     client_id=os.getenv("OAUTH2_CLIENT_ID"),
     client_secret=os.getenv("OAUTH2_CLIENT_SECRET"),
-    client_kwargs={
-        "scope": "https://www.googleapis.com/auth/youtube.upload",
-    },
-    server_metadata_url=f'{os.getenv("OAUTH2_META_URL")}',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'https://www.googleapis.com/auth/youtube.upload'},
 )
+
+def upload_video_to_youtube(credentials, file):
+    youtube = build('youtube', 'v3', credentials=credentials)
+    body = {
+        'snippet': {
+            'title': 'Test',
+            'description': 'Test',
+            'tags': 'Comedy, Funny, lol',
+            'categoryId': 22
+        },
+        'status': {
+            'privacyStatus': 'public'
+        }
+    }
+    media = MediaIoBaseUpload(BytesIO(file.read()), mimetype='video/*', resumable=True)
+    response = youtube.videos().insert(part='snippet,status', body=body, media_body=media)
+    response = response.execute()
+    return response
 
 @app.route("/")
 def home():
@@ -22,13 +46,28 @@ def home():
 
 @app.route("/google-login")
 def googleLogin():
-    return oauth.myApp.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
+    redirect_uri = url_for('googleCallback', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
 @app.route("/signin-google")
 def googleCallback():
-    token = oauth.myApp.authorize_access_token()
-    session["user"] = token
+    token = google.authorize_access_token()
+    session['user'] = token
     return redirect(url_for("home"))
+
+@app.route('/upload', methods=['POST'])
+def upload_video():
+    file = request.files['file']
+    token = session.get('user')
+    credentials = Credentials(
+        token=token.get('access_token'),
+        refresh_token=token.get('refresh_token'),
+        token_uri='https://oauth2.googleapis.com/token',
+        client_id=os.getenv('OAUTH2_CLIENT_ID'),
+        client_secret=os.getenv('OAUTH2_CLIENT_SECRET')
+    )
+    response = upload_video_to_youtube(credentials, file)
+    return "Done"
 
 @app.route("/logout")
 def logout():
