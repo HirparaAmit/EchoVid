bc7ad0ea16ba6579fd7efaa0f29a6344cf72041b
Amit Hirpara <hirparaamit43@gmail.com>
2023-12-29 12:48:16 +0530

    Update code

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask, url_for, session, redirect, request, render_template
+from flask import Flask, url_for, session, redirect, request, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from werkzeug.security import generate_password_hash, check_password_hash
 from authlib.integrations.flask_client import OAuth
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
@@ -7,6 +10,9 @@ from google.oauth2.credentials import Credentials
 from io import BytesIO
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRE_USER')}:{os.getenv('POSTGRE_PASSWORD')}@{os.getenv('POSTGRE_HOST')}/{os.getenv('POSTGRE_DATABASE')}"
+db = SQLAlchemy(app)
 
 app.secret_key = os.getenv("FLASK_SECRET")
 oauth = OAuth(app)
@@ -41,8 +47,50 @@ def upload_video_to_youtube(credentials, file):
     return response
 
 @app.route("/")
-def home():
-    return render_template("index.html", session=session.get("user"))
+def index():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("home.html")
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form. get('password')
+        email_check = text("SELECT COUNT(*) FROM public.users WHERE email = :email")
+        email_check = db.session.execute(email_check, {'email': email}).scalar()
+        if email_check > 0:
+            flash("Email already in use!")
+            return redirect(url_for('signup'))
+        else:
+            password = generate_password_hash(password)
+            sql_query = text("INSERT INTO public.users (email, password) VALUES (:email, :password)")
+            db.session.execute(sql_query, {"email": email, "password": password})
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        email_check = text("SELECT COUNT(*) FROM public.users WHERE email = :email")
+        email_check = db.session.execute(email_check, {'email': email}).scalar()
+        if email_check:
+            password_check = text("SELECT password FROM public.users WHERE email = :email")
+            password_check = db.session.execute(password_check, {'email': email}).scalar()
+            if password_check and check_password_hash(password_check, password):
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Wrong credentials!")
+                return redirect(url_for('login'))
+        else:
+            flash("Wrong credentials!")
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
 @app.route("/google-login")
 def googleLogin():
@@ -53,7 +101,7 @@ def googleLogin():
 def googleCallback():
     token = google.authorize_access_token()
     session['user'] = token
-    return redirect(url_for("home"))
+    return redirect(url_for("dashboard"))
 
 @app.route('/upload', methods=['POST'])
 def upload_video():
@@ -72,7 +120,7 @@ def upload_video():
 @app.route("/logout")
 def logout():
     session.pop("user", None)
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
